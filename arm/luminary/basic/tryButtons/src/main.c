bc81e721d8bbe5552afa43d98c06461f395df71c
pfl <pfl@eal.dk>
2012-02-16 12:55:26 +0100

    moved initialization and IO setup to initHW()

--- arm/luminary/basic/tryButtons/src/main.c
+++ arm/luminary/basic/tryButtons/src/main.c
@@ -5,7 +5,6 @@
  *      Author: pfl
  */
 
-
 #include "rit128x96x4.h"
 
 /* Hardware library includes. */
@@ -23,70 +22,79 @@
 #define mainFULL_SCALE						( 15 )
 #define ulSSI_FREQUENCY						( 3500000UL )
 
-int main(void) {
-	volatile unsigned long ulLoop;
+void
+initHW(void)
+{
+  volatile unsigned long ulLoop;
 
-	SysCtlClockSet(
-			SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN
-					| SYSCTL_XTAL_8MHZ);
+  SysCtlClockSet(
+      SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ);
 
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	GPIODirModeSet( GPIO_PORTF_BASE, GPIO_PIN_1,GPIO_DIR_MODE_IN );
-	GPIOPadConfigSet( GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU );
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+  GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_DIR_MODE_IN);
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA,
+      GPIO_PIN_TYPE_STD_WPU);
 
-	GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_DIR_MODE_OUT);
-	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA,
-				GPIO_PIN_TYPE_STD);
-	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 1);
+  GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_DIR_MODE_OUT);
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA,
+      GPIO_PIN_TYPE_STD);
+  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 1);
 
+  for (ulLoop = 0; ulLoop < 200; ulLoop++)
+    {
+    }
 
-	for (ulLoop = 0; ulLoop < 200; ulLoop++) {
-	}
+}
 
-	//
-	// Enable the GPIO pin for the LED (PF0).  Set the direction as output, and
-	// enable the GPIO pin for digital function.
-	//
+int
+main(void)
+{
+  volatile unsigned long ulLoop;
 
-	// Start the OLED display and wirte a message on it
+  initHW();
 
-	RIT128x96x4Init(ulSSI_FREQUENCY);
-	RIT128x96x4StringDraw("Hi :)", 0, 0, mainFULL_SCALE);
-	RIT128x96x4StringDraw("Press the ""select"" key..", 0, 5, mainFULL_SCALE);
+  // Start the OLED display and write a message on it
 
-	// Wait for the select key to be pressed
-	//GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1);
+  RIT128x96x4Init(ulSSI_FREQUENCY);
+  RIT128x96x4StringDraw("Hi :)", 0, 0, mainFULL_SCALE);
+  RIT128x96x4StringDraw("Press the " "select" " key..", 0, 5, mainFULL_SCALE);
 
+  // Wait for the select key to be pressed
+  //GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_1);
 
-	while( GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1) );
+  while (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1))
+    ;
 
-	//
-	// Loop forever.
-	//
-	while (1) {
-		//
-		// Turn on the LED.
-		//
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0);
-		// GPIO_PORTF_DATA_R |= 0x01;
+  //
+  // Loop forever.
+  //
+  while (1)
+    {
+      //
+      // Turn on the LED.
+      //
+      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0);
+      // GPIO_PORTF_DATA_R |= 0x01;
 
-		//
-		// Delay for a bit.
-		//
-		for (ulLoop = 0; ulLoop < 200000; ulLoop++) {
-		}
+      //
+      // Delay for a bit.
+      //
+      for (ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
 
-		//
-		// Turn off the LED.
-		//
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0);
+      //
+      // Turn off the LED.
+      //
+      GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0);
 
-		//
-		// Delay for a bit.
-		//
-		for (ulLoop = 0; ulLoop < 200000; ulLoop++) {
-		}
-	}
+      //
+      // Delay for a bit.
+      //
+      for (ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
+    }
 
-	return 0;
+  return 0;
 }
