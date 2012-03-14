bdc2c5bd18b55bdf9bc3f7252c1c37ab83ee3a98
flindt <flindt@flindt-X301>
2012-03-14 18:37:18 +0100

    cleaned up some comments

--- arm/luminary/basic/tryBuzzer/src/main.c
+++ arm/luminary/basic/tryBuzzer/src/main.c
@@ -169,22 +169,20 @@ void initPWM()
 	PWMGenConfigure(PWM_BASE, PWM_GEN_0,
 	PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
 	//
-	// Set the period. For a 50 KHz frequency, the period = 1/50,000, or 20
-	// microseconds. For a 20 MHz clock, this translates to 400 clock ticks.
-	// Use this value to set the period.
+	// Set the period.
 	//
 	PWMGenPeriodSet(PWM_BASE, PWM_GEN_0, 80000);
 	//
-	// Set the pulse width of PWM0 for a 25% duty cycle.
+	// Set the pulse width of PWM1
 	//
-	PWMPulseWidthSet(PWM_BASE, PWM_OUT_1, 40);
+	PWMPulseWidthSet(PWM_BASE, PWM_OUT_1, 40000);
 
 	//
 	// Start the timers in generator 0.
 	//
 	PWMGenEnable(PWM_BASE, PWM_GEN_0);
 	//
-	// Enable the outputs.
+	// Enable the output.
 	//
 	PWMOutputState(PWM_BASE, PWM_OUT_1_BIT, true);
 
@@ -195,8 +193,6 @@ void
 initHW(void)
 {
   volatile unsigned long ulLoop;
-
-
 
   SysCtlClockSet(
       SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_8MHZ);
