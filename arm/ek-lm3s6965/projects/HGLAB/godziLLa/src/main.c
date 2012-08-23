179ae4fe5bd37489032242099398a54e05da3e08
flindt <flindt@flindt-X301>
2012-08-23 18:08:41 +0200

    moved the key reading functions into its own module

--- arm/ek-lm3s6965/projects/HGLAB/godziLLa/src/main.c
+++ arm/ek-lm3s6965/projects/HGLAB/godziLLa/src/main.c
@@ -26,18 +26,12 @@
 #define mainFULL_SCALE						( 15 )
 #define ulSSI_FREQUENCY						( 3500000UL )
 
-// Stuff for the Key interface
-const int KEY_PRESS_MINIMUM = 7;
+
 
 // Function prototypes
 void
 initHW(void);
 
-
-int
-ReadKeys(void);
-int
-GetKeyEvents(void);
 
 // With this setup it would seem like main() must be the first function in this file, otherwise
 // the wrong function gets called on reset.
@@ -86,61 +80,7 @@ main(void)
   return 0;
 }
 
-// This function translates a set of bits read from the key IO ports into key events.
-// Adding more keys is a matter of adding a new enum value, and one reading
-// one more port in ReadKeys()
-int
-GetKeyEvents(void)
-{
-  static int KeyPressFlag = 0;
-  static int Count[NO_EVENT] =
-    { 0 };
-  int LoopCount;
-  int RawKeys;
 
-  RawKeys = ReadKeys();
-
-  for (LoopCount = 0; LoopCount < NO_EVENT; LoopCount++)
-    {
-      if (RawKeys & (1 << LoopCount))
-        {
-          Count[LoopCount]++;
-          if (( Count[LoopCount] >= KEY_PRESS_MINIMUM) && !(KeyPressFlag & (1<<LoopCount)))
-            {
-              KeyPressFlag |=  (1<<LoopCount);
-              Count[LoopCount] = 0;
-              return LoopCount;
-            }
-        }
-      else
-        {
-          KeyPressFlag &=  ~(1<<LoopCount);
-          Count[LoopCount] = 0;
-        }
-
-
-    }
-
-  return NO_EVENT;
-}
-
-// This function will read the actual io  ports associated with each key
-// This is the only function besides initHW, that will ever need to know
-// where each button is connected
-int
-ReadKeys(void)
-{
-  int KeyBits = 0;
-  if (!GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_0))
-    KeyBits |= 1 << KEY_UP;
-  if (!GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_1))
-    KeyBits |= 1 << KEY_DOWN;
-  if (!GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_2))
-    KeyBits |= 1 << KEY_CANCEL;
-  if (!GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3))
-    KeyBits |= 1 << KEY_ENTER;
-  return KeyBits;
-}
 
 void
 initHW(void)
