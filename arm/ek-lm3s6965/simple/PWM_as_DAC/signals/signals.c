d1f6b3f325855a6c2450f9389ec7bf1be6392abb
Flindt <pfl@eal.dk>
2012-11-13 09:46:15 +0100

    added signals module This will contain the DSP signal part of the code

---
+++ arm/ek-lm3s6965/simple/PWM_as_DAC/signals/signals.c
@@ -0,0 +1,12 @@
+/*
+ * signals.c
+ *
+ *  Created on: Nov 13, 2012
+ *      Author: flindt
+ */
+
+
+float calcNextOutputValue(){
+
+	return 0;
+}
---
+++ arm/ek-lm3s6965/simple/PWM_as_DAC/signals/signals.h
@@ -0,0 +1,13 @@
+/*
+ * signals.h
+ *
+ *  Created on: Nov 13, 2012
+ *      Author: flindt
+ */
+
+#ifndef SIGNALS_H_
+#define SIGNALS_H_
+
+float calcNextOutputValue();
+
+#endif /* SIGNALS_H_ */
