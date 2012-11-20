0d49bdc3c81177a092d1ab8566fb64f5bcd07c79
Flindt <pfl@eal.dk>
2012-11-20 07:46:53 +0100

    added settings.h for project wide settings added a few constants

---
+++ arm/ek-lm3s6965/simple/PWM_as_DAC/src/settings.h
@@ -0,0 +1,16 @@
+/*
+ * settings.h
+ *
+ * 	Contains global constants
+ *
+ *  Created on: Nov 20, 2012
+ *      Author: flindt
+ */
+
+
+#ifndef SETTINGS_H_
+#define SETTINGS_H_
+
+	const int SAMPLE_RATE = 100; // Hz
+
+#endif /* SETTINGS_H_ */
