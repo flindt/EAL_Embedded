6f6c89ca33bfc7e54c0cd5c3d1e7f1eeefc66b5a
Lasse <Lasse@Lasse-SamsungPc>
2013-02-04 12:01:01 +0100

    CAN bus tx exp send 8 bit not emty

--- arm/ek-lm3s6965/projects/Can_Bus/main.c
+++ arm/ek-lm3s6965/projects/Can_Bus/main.c
@@ -1,8 +1,8 @@
 //*****************************************************************************
 //
-// blinky.c - Simple example to blink the on-board LED.
+// simple_tx.c - Example demonstrating simple CAN message transmission.
 //
-// Copyright (c) 2007-2012 Texas Instruments Incorporated.  All rights reserved.
+// Copyright (c) 2010-2012 Texas Instruments Incorporated.  All rights reserved.
 // Software License Agreement
 //
 // Texas Instruments (TI) is supplying this software for use solely and
@@ -18,75 +18,359 @@
 // CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
 // DAMAGES, FOR ANY REASON WHATSOEVER.
 //
-// This is part of revision 9107 of the EK-LM3S6965 Firmware Package.
+// This is part of revision 9107 of the Stellaris Firmware Development Package.
 //
 //*****************************************************************************
 
-#include "inc/lm3s9b92.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "inc/hw_can.h"
+#include "inc/hw_ints.h"
+#include "driverlib/can.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "utils/uartstdio.h"
+#include "inc/lm3s9b92.h"
 //*****************************************************************************
 //
-//! \addtogroup example_list
-//! <h1>Blinky (blinky)</h1>
+//! \addtogroup can_examples_list
+//! <h1>Simple CAN TX (simple_tx)</h1>
 //!
-//! A very simple example that blinks the on-board LED.
+//! This example shows the basic setup of CAN in order to transmit messages
+//! on the CAN bus.  The CAN peripheral is configured to transmit messages
+//! with a specific CAN ID.  A message is then transmitted once per second,
+//! using a simple delay loop for timing.  The message that is sent is a 4
+//! byte message that contains an incrementing pattern.  A CAN interrupt
+//! handler is used to confirm message transmission and count the number of
+//! messages that have been sent.
+//!
+//! This example uses the following peripherals and I/O signals.  You must
+//! review these and change as needed for your own board:
+//! - CAN0 peripheral
+//! - GPIO Port D peripheral (for CAN0 pins)
+//! - CAN0RX - PD0
+//! - CAN0TX - PD1
+//!
+//! The following UART signals are configured only for displaying console
+//! messages for this example.  These are not required for operation of CAN.
+//! - GPIO port A peripheral (for UART0 pins)
+//! - UART0RX - PA0
+//! - UART0TX - PA1
+//!
+//! This example uses the following interrupt handlers.  To use this example
+//! in your own application you must add these interrupt handlers to your
+//! vector table.
+//! - INT_CAN0 - CANIntHandler
+//!
 //
 //*****************************************************************************
 
 //*****************************************************************************
 //
-// Blink the on-board LED.
+// A counter that keeps track of the number of times the TX interrupt has
+// occurred, which should match the number of TX messages that were sent.
 //
 //*****************************************************************************
-int
-main(void)
+volatile unsigned long g_ulMsgCount = 0;
+
+//*****************************************************************************
+//
+// A flag to indicate that some transmission error occurred.
+//
+//*****************************************************************************
+volatile unsigned long g_bErrFlag = 0;
+
+//*****************************************************************************
+//
+// This function sets up UART0 to be used for a console to display information
+// as the example is running.
+//
+//*****************************************************************************
+/*
+void
+InitConsole(void)
 {
-    volatile unsigned long ulLoop;
+    //
+    // Enable GPIO port A which is used for UART0 pins
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     //
-    // Enable the GPIO port that is used for the on-board LED.
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
     //
-    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOD;
+    GPIOPinConfigure(0x00000001);
+    GPIOPinConfigure(0x00000401);
 
     //
-    // Do a dummy read to insert a few cycles after enabling the peripheral.
+    // Select the alternate (UART) function for these pins.
     //
-    ulLoop = SYSCTL_RCGC2_R;
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     //
-    // Enable the GPIO pin for the LED (PF0).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
+    // Initialize the UART for console I/O.
     //
-    GPIO_PORTD_DIR_R = 0x01;
-    GPIO_PORTD_DEN_R = 0x01;
+    UARTStdioInit(0);
+}
+*/
+
+//*****************************************************************************
+//
+// This function provides a 1 second delay using a simple polling method.
+//
+//*****************************************************************************
+void
+SimpleDelay(void)
+{
+    //
+    // Delay cycles for 1 second
+    //
+    SysCtlDelay(16000000 / 3);
+}
+
+//*****************************************************************************
+//
+// This function is the interrupt handler for the CAN peripheral.  It checks
+// for the cause of the interrupt, and maintains a count of all messages that
+// have been transmitted.
+//
+//*****************************************************************************
+void
+CANIntHandler(void)
+{
+    unsigned long ulStatus;
 
     //
-    // Loop forever.
+    // Read the CAN interrupt status to find the cause of the interrupt
     //
-    while(1)
+    ulStatus = CANIntStatus(CAN0_BASE, CAN_INT_STS_CAUSE);
+
+    //
+    // If the cause is a controller status interrupt, then get the status
+    //
+    if(ulStatus == CAN_INT_INTID_STATUS)
     {
         //
-        // Turn on the LED.
+        // Read the controller status.  This will return a field of status
+        // error bits that can indicate various errors.  Error processing
+        // is not done in this example for simplicity.  Refer to the
+        // API documentation for details about the error status bits.
+        // The act of reading this status will clear the interrupt.  If the
+        // CAN peripheral is not connected to a CAN bus with other CAN devices
+        // present, then errors will occur and will be indicated in the
+        // controller status.
         //
-        GPIO_PORTD_DATA_R |= 0x01;
+        ulStatus = CANStatusGet(CAN0_BASE, CAN_STS_CONTROL);
 
         //
-        // Delay for a bit.
+        // Set a flag to indicate some errors may have occurred.
         //
-        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
-        {
-        }
+        g_bErrFlag = 1;
+    }
+
+    //
+    // Check if the cause is message object 1, which what we are using for
+    // sending messages.
+    //
+    else if(ulStatus == 1)
+    {
+        //
+        // Getting to this point means that the TX interrupt occurred on
+        // message object 1, and the message TX is complete.  Clear the
+        // message object interrupt.
+        //
+        CANIntClear(CAN0_BASE, 1);
 
         //
-        // Turn off the LED.
+        // Increment a counter to keep track of how many messages have been
+        // sent.  In a real application this could be used to set flags to
+        // indicate when a message is sent.
         //
-        GPIO_PORTD_DATA_R &= ~(0x01);
+        g_ulMsgCount++;
 
         //
-        // Delay for a bit.
+        // Since the message was sent, clear any error flags.
         //
-        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
-        {
-        }
+        g_bErrFlag = 0;
+    }
+
+    //
+    // Otherwise, something unexpected caused the interrupt.  This should
+    // never happen.
+    //
+    else
+    {
+        //
+        // Spurious interrupt handling can go here.
+        //
     }
 }
+
+//*****************************************************************************
+//
+// Configure the CAN and enter a loop to transmit periodic CAN messages.
+//
+//*****************************************************************************
+int main(void)
+{
+    tCANMsgObject sCANMessage;
+    unsigned char ucMsgData[8];
+
+    //
+    // Set the clocking to run directly from the external crystal/oscillator.
+    // TODO: The SYSCTL_XTAL_ value must be changed to match the value of the
+    // crystal on your board.
+    //
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+
+    //
+    // Set up the serial console to use for displaying messages.  This is
+    // just for this example program and is not needed for CAN operation.
+    //
+   // InitConsole();
+
+    //
+    // For this example CAN0 is used with RX and TX pins on port D0 and D1.
+    // The actual port and pins used may be different on your part, consult
+    // the data sheet for more information.
+    // GPIO port D needs to be enabled so these pins can be used.
+    // TODO: change this to whichever GPIO port you are using
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Configure the GPIO pin muxing to select CAN0 functions for these pins.
+    // This step selects which alternate function is available for these pins.
+    // This is necessary if your part supports GPIO pin function muxing.
+    // Consult the data sheet to see which functions are allocated per pin.
+    // TODO: change this to select the port/pin you are using
+    //
+    GPIOPinConfigure(0x00001005);
+    GPIOPinConfigure(0x00001405);
+
+    //
+    // Enable the alternate function on the GPIO pins.  The above step selects
+    // which alternate function is available.  This step actually enables the
+    // alternate function instead of GPIO for these pins.
+    // TODO: change this to match the port/pin you are using
+    //
+    GPIOPinTypeCAN(GPIO_PORTA_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+
+    //
+    // The GPIO port and pins have been set up for CAN.  The CAN peripheral
+    // must be enabled.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
+
+    //
+    // Initialize the CAN controller
+    //
+    CANInit(CAN0_BASE);
+
+    //
+    // Set up the bit rate for the CAN bus.  This function sets up the CAN
+    // bus timing for a nominal configuration.  You can achieve more control
+    // over the CAN bus timing by using the function CANBitTimingSet() instead
+    // of this one, if needed.
+    // In this example, the CAN bus is set to 500 kHz.  In the function below,
+    // the call to SysCtlClockGet() is used to determine the clock rate that
+    // is used for clocking the CAN peripheral.  This can be replaced with a
+    // fixed value if you know the value of the system clock, saving the extra
+    // function call.  For some parts, the CAN peripheral is clocked by a fixed
+    // 8 MHz regardless of the system clock in which case the call to
+    // SysCtlClockGet() should be replaced with 8000000.  Consult the data
+    // sheet for more information about CAN peripheral clocking.
+    //
+    CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 500000);
+
+    //
+    // Enable interrupts on the CAN peripheral.  This example uses static
+    // allocation of interrupt handlers which means the name of the handler
+    // is in the vector table of startup code.  If you want to use dynamic
+    // allocation of the vector table, then you must also call CANIntRegister()
+    // here.
+    //
+    // CANIntRegister(CAN0_BASE, CANIntHandler); // if using dynamic vectors
+    //
+    CANIntEnable(CAN0_BASE, CAN_INT_MASTER | CAN_INT_ERROR | CAN_INT_STATUS);
+
+    //
+    // Enable the CAN interrupt on the processor (NVIC).
+    //
+    IntEnable(INT_CAN0);
+
+    //
+    // Enable the CAN for operation.
+    //
+    CANEnable(CAN0_BASE);
+
+    //
+    // Initialize the message object that will be used for sending CAN
+    // messages.  The message will be 4 bytes that will contain an incrementing
+    // value.  Initially it will be set to 0.
+    //
+    *(unsigned long *)ucMsgData = 0;
+    sCANMessage.ulMsgID =0x7DF;                        // CAN message ID - use 1
+    sCANMessage.ulMsgIDMask = 0;                    // no mask needed for TX
+    sCANMessage.ulFlags = MSG_OBJ_TX_INT_ENABLE;    // enable interrupt on TX
+    sCANMessage.ulMsgLen = sizeof(ucMsgData);       // size of message is 4
+    sCANMessage.pucMsgData = ucMsgData;             // ptr to message content
+
+    //
+    // Enter loop to send messages.  A new message will be sent once per
+    // second.  The 4 bytes of message content will be treated as an unsigned
+    // long and incremented by one each time.
+    //
+    for(;;)
+    {
+        //
+        // Print a message to the console showing the message count and the
+        // contents of the message being sent.
+        //
+    	/*
+        UARTprintf("Sending msg: 0x%02X %02X %02X %02X",
+                   ucMsgData[0], ucMsgData[1], ucMsgData[2], ucMsgData[3]);
+*/
+        //
+        // Send the CAN message using object number 1 (not the same thing as
+        // CAN ID, which is also 1 in this example).  This function will cause
+        // the message to be transmitted right away.
+        //
+        CANMessageSet(CAN0_BASE, 1, &sCANMessage, MSG_OBJ_TYPE_TX);
+
+        //
+        // Now wait 1 second before continuing
+        //
+        SimpleDelay();
+
+        //
+        // Check the error flag to see if errors occurred
+        //
+/*
+        if(g_bErrFlag)
+        {
+            UARTprintf(" error - cable connected?\n");
+        }
+        else
+        {
+            //
+            // If no errors then print the count of message sent
+            //
+            UARTprintf(" total count = %u\n", g_ulMsgCount);
+        }
+
+        //
+        // Increment the value in the message data.
+        //
+        (*(unsigned long *)ucMsgData)++;
+*/
+    }
+
+    //
+    // Return no errors
+    //
+    return(0);
+}
+
