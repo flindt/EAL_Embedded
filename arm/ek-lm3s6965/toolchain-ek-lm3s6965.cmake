17619c3008fbc4b17879f1b1f7ee48cc7987701d
flindt <flindt@flindtathome.dk>
2015-09-24 20:46:39 +0200

    closer

--- arm/ek-lm3s6965/toolchain-ek-lm3s6965.cmake
+++ arm/ek-lm3s6965/toolchain-ek-lm3s6965.cmake
@@ -1,23 +1,18 @@
-
 INCLUDE(CMakeForceCompiler)
 
-# this one is important
-SET(CMAKE_SYSTEM_NAME generic)
+SET(CMAKE_SYSTEM_NAME Generic)
+SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   arm-none-eabi-gcc)
-SET(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+CMAKE_FORCE_C_COMPILER(arm-none-eabi-gcc GNU)
+CMAKE_FORCE_CXX_COMPILER(arm-none-eabi-g++ GNU)
 
 SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS)
 
-set(CMAKE_C_FLAGS "-mthumb             \
-       -mcpu=cortex-m3     \
-       -Os                 \
-       -ffunction-sections \
-       -fdata-sections     \
-       -MD                 \
-       -std=c99            \
-       -Wall               \
-       -pedantic           \
-       -DPART_LM3S6965      \
-       -c" )
+SET(COMMON_FLAGS "-mcpu=cortex-m3 -mthumb -mthumb-interwork \
+	-ffunction-sections -fdata-sections \
+	-Wall -pedantic -c -Os\
+	-DPART_LM3S6965")
+SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS}  -std=gnu++0x")
+SET(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections ")
